b8b09b2b3b345b84185fc0d07168483044df7410
GuinhoAntonio-dev <137727724+GuinhoAntonio-dev@users.noreply.github.com>
2023-06-28 18:49:15 +0100

    Create config.js

---
+++ config.js
@@ -0,0 +1,5 @@
+var config = {
+    namewebsite : "Meu website", //Nome do seu website
+    seunome : "seu nome", //Seu Nome
+    email: "seuemail@example.com" //Seu email publico para entrar em contato
+  };
